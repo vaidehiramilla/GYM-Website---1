3d0c1d8528bdf8f21c19a0d4a31338ba7b257d7b
shivaram7721 <shivaramdusa7721@gmail.com>
2023-06-07 12:56:33 +0530

    build

--- src/pages/activity/day.jsx
+++ src/pages/activity/day.jsx
@@ -2,7 +2,7 @@ import styles from './Day.module.css'
 
 export function Day() {
   return (
-    <div className={styles.dayContainer}>
+    <div>
       <h1 style={{ margin: "100px" }}>Day plan</h1>
 
       <div>
